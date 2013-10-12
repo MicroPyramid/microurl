295a5c571bc1482bb5b0107f535956beb8a6ab42
aswanii <aswanii@gmail.com>
2013-10-12 20:31:33 +0530

    change of names and planned bitly usage

--- mini.py
+++ mini.py
@@ -2,7 +2,8 @@ import urllib2
 import json
 from conf import Google_API_KEY
 
-def google(url):
+# This will minify url using google url minifier and return minified url
+def google_mini(url):
     post_url = 'https://www.googleapis.com/urlshortener/v1/url?key='+Google_API_KEY
     postdata = {'longUrl':url}
     headers = {'Content-Type':'application/json'}
@@ -13,3 +14,13 @@ def google(url):
     )
     ret = urllib2.urlopen(req).read()
     return json.loads(ret)['id']
+
+
+# This will stats of the minified url using google url shortner
+def google_stat(min_url)
+    pass
+
+
+# This will minify url using bily
+def bitly_mini(url)
+    pass
