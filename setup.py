a5d0564dd35c8c4c63e827759de4a8860f3a0a68
aswanii <aswanii@gmail.com>
2013-10-13 16:06:43 +0530

    updated package info

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-#
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='microurl',
+      version='0.0.1',
+      #install_requires=['lxml', 'PIL'],
+      description='microurl is a python module to create minfied urls',
+      author='MicroPyramid',
+      author_email='MicroPyramid@googlegroups.com',
+      maintainer='Archana, Ashwin',
+      maintainer_email='MicroPyramid@googlegroups.com',
+      url='http://github.com/micropyramid/microurl',
+      py_modules=['microurl'],
+      
+      license=open('LICENSE').read(),
+      long_description=open('README.md').read(),
+      
+      '''
+      requires=[
+        'Django(>=1.3)',
+        'pydns',
+      ],
+      '''
+      
+      download_url = 'https://github.com/MicroPyramid/microurl.git',
+      keywords = 'Url minifier python library using google, bitly, supr, dottk',
+      )
+      
+      
+      
+      
