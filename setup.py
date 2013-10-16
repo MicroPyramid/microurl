96250e8dc8833801c9421d57f25a41fcc2324190
aswanii <aswanii@gmail.com>
2013-10-16 10:52:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
       description='microurl is a python module to create minfied urls',
       author='MicroPyramid',
       author_email='MicroPyramid@googlegroups.com',
-      maintainer='Archana, Ashwin',
+      maintainer='Ashwin',
       maintainer_email='MicroPyramid@googlegroups.com',
       url='http://github.com/micropyramid/microurl',
 
       license='GPL3',
       long_description=open('README.rst').read(),
 
-      keywords = 'Url minifier python library using google, bitly, supr, dottk',
+      keywords = 'Url minifier python library using google, bitly',
       packages=['microurl'],
       include_package_data=True,
       zip_safe=True,
