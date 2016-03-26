aa890a7e8b4e60fa9ce001e1c138791ae3b8e410
ravi <ravi@micropyramid.com>
2016-03-26 18:18:04 +0530

    raise Exception when response in not 200 from google

--- microurl/google.py
+++ microurl/google.py
@@ -8,4 +8,8 @@ def google_mini(url, Google_API_KEY):
     payload = {'longUrl': url}
     headers = {'content-type': 'application/json'}
     r = requests.post(post_url, data=json.dumps(payload), headers=headers)
-    return json.loads(r.text)['id']
+    if r.status_code == 200:
+        return r.json().get('id')
+    elif r.status_code == 400:
+        raise KeyError('Can not Generate Minified url, Reason:', r.json().get('error', {}).get('errors')[0].get('reason'))
+    raise KeyError('Invalid Google Api Key or url')
