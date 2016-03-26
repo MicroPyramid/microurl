7fa6b715b74624c8f14950a24037ced1ec06fd95
rakesh <rakesh@micropyramid.com>
2016-03-26 17:41:16 +0530

    QR generator and google expander added

--- microurl/google.py
+++ microurl/google.py
@@ -9,3 +9,15 @@ def google_mini(url, Google_API_KEY):
     headers = {'content-type': 'application/json'}
     r = requests.post(post_url, data=json.dumps(payload), headers=headers)
     return json.loads(r.text)['id']
+
+
+def google_expand(short_url, Google_API_KEY):
+    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + \
+        Google_API_KEY
+    payload = {'shortUrl': short_url}
+    r = requests.get(post_url, params=payload)
+    return r.json()['longUrl']
+
+
+def qrcode(url):
+    return 'http://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=' + url
