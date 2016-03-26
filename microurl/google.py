e2d7ed2c6716744acaeee225ac338ca363c1ab9e
rakesh <rakesh@micropyramid.com>
2016-03-26 17:59:16 +0530

    exception handling added

--- microurl/google.py
+++ microurl/google.py
@@ -7,16 +7,22 @@ def google_mini(url, Google_API_KEY):
         Google_API_KEY
     payload = {'longUrl': url}
     headers = {'content-type': 'application/json'}
-    r = requests.post(post_url, data=json.dumps(payload), headers=headers)
-    return json.loads(r.text)['id']
+    try:
+        r = requests.post(post_url, data=json.dumps(payload), headers=headers)
+        return json.loads(r.text)['id']
+    except:
+        raise Exception('Problem occured while generating shorturl')
 
 
 def google_expand(short_url, Google_API_KEY):
     post_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + \
         Google_API_KEY
     payload = {'shortUrl': short_url}
-    r = requests.get(post_url, params=payload)
-    return r.json()['longUrl']
+    try:
+        r = requests.get(post_url, params=payload)
+        return r.json()['longUrl']
+    except:
+        raise Exception('The shorturl does not exist')
 
 
 def qrcode(url):
