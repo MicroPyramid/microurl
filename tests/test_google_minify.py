f3dc212ed2c51b79a6bd984a2992fed330ee6afd
Julien Chaumont <julienc91@users.noreply.github.com>
2017-10-24 03:50:45 +0200

    New test case for Google shortenerAPI (#16)
    * Added python 3.6 in travis.yml
    
    * added integration test for google expand

--- tests/test_google_minify.py
+++ tests/test_google_minify.py
@@ -1,5 +1,5 @@
 import unittest
-from microurl.google import google_mini
+from microurl.google import google_mini, google_expand
 
 GOOGLE_API_KEY = 'AIzaSyB_nDH7Uvm6-KSbsJD_OqYXA2XmuZ1P1lE'
 
@@ -22,6 +22,19 @@ class Testwrongurl(unittest.TestCase):
     def test_wrong_url(self):
         with self.assertRaises(KeyError):
             google_mini('wrong urlpattern', GOOGLE_API_KEY)
+
+
+class TestMinifyAndExpand(unittest.TestCase):
+
+    def test_minify_and_expand(self):
+        test_url = 'https://micropyramid.com/'
+
+        minified_url = google_mini(test_url, GOOGLE_API_KEY)
+        self.assertTrue(minified_url)
+
+        expanded_url = google_expand(minified_url, GOOGLE_API_KEY)
+        self.assertEqual(test_url, expanded_url)
+
 
 if __name__ == '__main__':
     unittest.main()
